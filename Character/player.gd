4f3f72b9c771a4c31c10de116c77e4ae834304b3
jacobbailey101 <88062523+jacobbailey101@users.noreply.github.com>
2023-06-14 09:05:58 +1200

    Animation
    I finished the jump animation and added the animation change when you switch from running to jumping

--- Character/player.gd
+++ Character/player.gd
@@ -10,21 +10,28 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
+var was_in_air :bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
+	else:
+		if was_in_air == true:
+			land()
+			
+		was_in_air = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_vector("left", "right", "up", "down")
 	
-	if direction:
+	if direction.x != 0 && animated_sprite.animation != "jump_end":
 		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
@@ -35,10 +42,13 @@ func _physics_process(delta):
 	
 func update_animation():
 	if not animation_locked:
-		if direction.x != 0:
-			animated_sprite.play("run")
+		if not is_on_floor():
+			animated_sprite.play("jump_loop")
 		else:
-			animated_sprite.play("idle")
+			if direction.x != 0:
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("idle")
 			
 func update_facing_direction():
 	if direction .x > 0:
@@ -46,4 +56,19 @@ func update_facing_direction():
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
 		
+func jump():
+	velocity.y = jump_velocity
+	animated_sprite.play("jump_start")
+	animation_locked = true
+	
+func land():
+	animated_sprite.play("jump_end")
+	animation_locked = true
 		
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if(animated_sprite.animation == "jump_end"):
+		animation_locked = false
+	elif(animated_sprite.animation == "jump_start"):
+		animation_locked = false
